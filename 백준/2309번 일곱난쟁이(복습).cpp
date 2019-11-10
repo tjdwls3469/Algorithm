4e49aaafb916a1bbb3e23792471e9cf2227aecfd
kimsungjin <tjdwls3469@naver.com>
2019-11-10 13:24:46 +0900

    [ISSUE-32] 2309번 일곱난쟁이(복습)  - 2309번 일곱난쟁이 풀이(복습)

---
+++ 백준/2309번 일곱난쟁이(복습).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int dwarf[9];
+	int sum = 0;
+	for (int i = 0; i < 9; i++) {
+		cin >> dwarf[i];
+		sum += dwarf[i];
+	}
+	sort(dwarf, dwarf + 9);
+	for (int i = 0; i < 9; i++) {
+		for (int j = i + 1; j < 9; j++) {
+			if (sum - (dwarf[i] + dwarf[j]) == 100) {
+				for (int k = 0; k < 9; k++) {
+					if (k == i || k == j) {
+						continue;
+					}
+					cout << dwarf[k] << '\n';
+				}
+				return 0;
+			}
+		}
+	}
+
+	return 0;
+}
