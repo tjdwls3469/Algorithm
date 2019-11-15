634fee4f2bf3d5524322299f1e293e9cca46332e
kimsungjin <tjdwls3469@naver.com>
2019-11-15 09:34:47 +0900

    [ISSUE-45] 10972번 다음 순열  - 10972번 다음 순열 풀이

---
+++ 백준/10972번 다음 순열.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int cnt;
+	cin >> cnt;
+	vector<int> number(cnt);
+	for (int i = 0; i < cnt; i++) {
+		cin >> number[i];
+	}
+	if (next_permutation(number.begin(), number.end()) == true) {
+		for (int i = 0; i < cnt; i++) {
+			cout << number[i] << ' ';
+		}
+	}
+	else {
+		cout << -1;
+	}
+	
+
+	return 0;
+}
