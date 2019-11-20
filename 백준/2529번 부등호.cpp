deef4fec0a2dfdca9a49182b31eba94f1b4d5648
kimsungjin <tjdwls3469@naver.com>
2019-11-20 17:45:26 +0900

    [ISSUE-58] 2529번 부등호  - 2529번 부등호 풀이

--- 백준/2529번 부등호.cpp
+++ 백준/2529번 부등호.cpp
@@ -1,32 +1,48 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
 using namespace std;
-
+bool check(vector<int> &perm, vector<char> &a) {
+	for (int i = 0; i < a.size(); i++) {
+		if (a[i] == '<' && perm[i] > perm[i + 1]) {
+			return false;
+		}
+		if (a[i] == '>' && perm[i] < perm[i + 1]) {
+			return false;
+		}
+	}
+	return true;
+}
 int main() {
-	int cnt;
-	cin >> cnt;
-	vector<char> sign(cnt);
-	vector<int> minAry(cnt + 1);
-	vector<int> maxAry(cnt + 1);
-	for (int i = 0; i < cnt; i++) {
-		cin >> sign[i];
+	int k;
+	cin >> k;
+	vector<char> a(k);
+	for (int i = 0; i < k; i++) {
+		cin >> a[i];
 	}
-	for (int i = 0; i <= cnt; i++) {
-		minAry[i] = i;
-		maxAry[i] = 9 - i;
+	vector<int> small(k + 1);
+	vector<int> big(k + 1);
+	for (int i = 0; i <= k; i++) {
+		small[i] = i;
+		big[i] = 9 - i;
 	}
-	int max = 0;
-	int min = 999999999;
-	for (int i = 0; i < cnt; i++) {
-		if (sign[i] == '<') {
-			minAry[i]
+	do {
+		if (check(small, a)) {
+			break;
 		}
-		else {
-		
+	} while (next_permutation(small.begin(), small.end()));
+	do {
+		if (check(big, a)) {
+			break;
 		}
+	} while (prev_permutation(big.begin(), big.end()));
+	for (int i = 0; i < big.size(); i++) {
+		cout << big[i];
 	}
-
-
+	cout << '\n';
+	for (int i = 0; i < small.size(); i++) {
+		cout << small[i];
+	}
+	cout << '\n';
 	return 0;
 }
