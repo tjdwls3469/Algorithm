e46bc06968c717326ba64aad7bdcf73a2fa1a133
kimsungjin <tjdwls3469@naver.com>
2020-01-09 08:45:21 +0900

    [ISSUE-98] 1978번 소수 찾기  - 1978번 소수 찾기 풀이

---
+++ 백준/1978번 소수 찾기.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+bool prime(int num) {
+	if (num < 2) {
+		return false;
+	}
+	for (int i = 2; i*i <= num; i++) {
+		if (num % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	int cnt;
+	int num;
+	int ans = 0;
+	cin >> cnt;
+	for (int i = 0; i < cnt; i++) {
+		cin >> num;
+		if (prime(num) == true) {
+			ans++;
+		}
+	}
+	cout << ans;
+	return 0;
+}
