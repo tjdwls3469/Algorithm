06015aba8941b07d0a17b609371ff41c82babb20
kimsungjin <tjdwls3469@naver.com>
2020-01-09 09:06:50 +0900

    [ISSUE-99] 10973번 이전 순열  - 10973번 이전 순열 풀이

---
+++ 백준/10973번 이전 순열(풀이).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int cnt;
+	cin >> cnt;
+	vector<int> num(cnt);
+	for (int i = 0; i < cnt; i++) {
+		cin >> num[i];
+	}
+	if (prev_permutation(num.begin(), num.end()) == true) {
+		for (int i = 0; i < cnt; i++) {
+			cout << num[i] << ' ';
+		}
+	}
+	else {
+		cout << -1;
+	}
+	return 0;
+}
