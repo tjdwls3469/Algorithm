daa219c34ddf478c484e63400f4bd6800b32a527
kimsungjin <tjdwls3469@naver.com>
2020-04-18 09:50:40 +0900

    [ISSUE-207] 4963번 섬의 개수  - 4963번 섬의 개수 풀이

---
+++ 백준/4963번 섬의 개수(스터디).cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+int col;
+int row;
+int map[50][50];
+bool check[50][50];
+int dx[] = { 0,-1,-1,-1,0,1,1,1 };
+int dy[] = { -1,-1,0,1,1,1,0,-1 };
+
+void bfs(int x, int y) {
+	queue<pair<int, int>> q;
+	check[x][y] = true;
+	q.push(make_pair(x, y));
+	while (!q.empty()) {
+		int fx = q.front().first;
+		int fy = q.front().second;
+		q.pop();
+		for (int i = 0; i < 8; i++) {
+			int nx = fx + dx[i];
+			int ny = fy + dy[i];
+			if (nx >= 0 && nx < row && ny >= 0 && ny < col) {
+				if (map[nx][ny] == 1 && check[nx][ny] == false) {
+					check[nx][ny] = true;
+					q.push(make_pair(nx, ny));
+				}
+			}
+		}
+	}
+}
+
+int main() {
+	cin.sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	while (true) {
+		int ans = 0;
+		cin >> col >> row;
+		if (col == 0 && row == 0) {
+			break;
+		}
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				cin >> map[i][j];
+			}
+		}
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				if (map[i][j] == 1 && check[i][j] == false) {
+					ans++;
+					bfs(i, j);
+				}
+			}
+		}
+		cout << ans << '\n';
+		memset(map, 0, sizeof(map));
+		memset(check, false, sizeof(check));
+	}
+	return 0;
+}
