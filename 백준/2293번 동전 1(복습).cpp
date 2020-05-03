aa91c4496725cffe4ee1dfae2c214183bcfa0476
kimsungjin <tjdwls3469@naver.com>
2020-05-03 12:53:55 +0900

    [ISSUE-236] 2293번 동전 1  - 2293번 동전 1 풀이

---
+++ 백준/2293번 동전 1(복습).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int coin[101];
+long long ans[10001];
+
+int main() {
+	cin.sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int cnt;
+	int goal;
+	cin >> cnt >> goal;
+	for (int i = 1; i <= cnt; i++) {
+		cin >> coin[i];
+	}
+	sort(coin, coin + cnt + 1);
+	ans[0] = 1;
+	for (int j = 1; j <= cnt; j++) {
+		for (int i = coin[j]; i <= goal; i++) {
+			ans[i] += ans[i - coin[j]];
+		}
+	}
+	cout << ans[goal];
+	return 0;
+}
